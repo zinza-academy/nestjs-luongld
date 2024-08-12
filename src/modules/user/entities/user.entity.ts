c886283b514fa9742540ab40f53e9f4ae514acfa
luongld <luong.ld@zinza.com.vn>
2024-08-12 09:58:24 +0700

    feat: create User entity

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  age: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
